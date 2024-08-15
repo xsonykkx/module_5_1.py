5aa0524ed963dbc8738a1872c794edb4766fcf8e
xsonykkx <sofiya.strologo@gmail.com>
2024-08-15 18:31:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        for i in range(self.number_of_floors):
+            print()
+            return
+        int(new_floor)
+        if new_floor < 1:
+            print("Такого этажа не существует")
+        elif new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            print(new_floor)
+     def __len__(self):
+         return self.number_of_floors
+     def __str__(self):
+         return "Название: {0}, кол-во этажей: {1}".format(self.name, self.number_of_floors)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
+
+
+
+
+
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+#
+# # __str__
+# print(h1)
+# print(h2)
+#
+# # __len__
+# print(len(h1))
+# print(len(h2))
+
+
+
+
