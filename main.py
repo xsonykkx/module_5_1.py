7857c18ec1d5bd4fcf3fdbaccda8091ed8ef04cb
xsonykkx <sofiya.strologo@gmail.com>
2024-08-31 12:39:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
     def go_to(self, new_floor):
-        for i in range(self.number_of_floors):
-            print()
-            return
-        int(new_floor)
-        if new_floor < 1:
-            print("Такого этажа не существует")
-        elif new_floor > self.number_of_floors:
-            print("Такого этажа не существует")
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
         else:
-            print(new_floor)
+            print("Такого этажа не существует")
+
     def __len__(self):
          return self.number_of_floors
     def __str__(self):
